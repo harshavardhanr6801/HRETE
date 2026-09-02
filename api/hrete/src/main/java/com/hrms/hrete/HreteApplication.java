fa6a042e60c5000d7d0e59138d4e3d6450c4d0bb
harshavardhanr6801 <harshavardhanr6801@gmail.com>
2026-09-02 13:20:03 +0530

    Initial commit

---
+++ api/hrete/src/main/java/com/hrms/hrete/HreteApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.hrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HreteApplication.class, args);
+	}
+
+}
---
+++ api/hrete/src/test/java/com/hrms/hrete/HreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.hrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
