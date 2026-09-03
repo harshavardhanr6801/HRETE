7c8f29d30de3268c65f6ab9c3352b31747ebe8d2
harshavardhanr6801 <harshavardhanr6801@gmail.com>
2026-09-03 10:30:43 +0530

    feat(config): add mapstruct dependency and create the auditing fields to map into the schemas

--- api/hrete/src/main/java/com/hrms/hrete/HreteApplication.java
+++ api/hrete/src/main/java/com/hrms/hrete/HreteApplication.java
@@ -2,8 +2,10 @@ package com.hrms.hrete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HreteApplication {
 
 	public static void main(String[] args) {
